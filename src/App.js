7d811df17a5648f98dd86c9d864e6b12ea5883a4
MohammedMaher177 <anahammo777@gmail.com>
2023-07-16 16:09:10 +0300

    firest

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './Components/Home/Home.jsx';
+import Posts from './Components/Posts/Posts.jsx';
+import Movies from './Components/Movies/Movies.jsx';
+import NotFound from './Components/NotFound/NotFound.jsx';
+import Layout from './Components/Layout/Layout.jsx';
+import Login from './Components/Login/Login.jsx';
+import Register from './Components/Register/Register.jsx';
+import { Provider } from 'react-redux';
+import store from './Redux/Store.js';
+import Profile from './Components/Profile/Profile.jsx';
+import PostWithComments from './Components/Posts/PostWithComments.jsx';
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+
+    children: [
+      { path: "team", element: <Navbar />, },
+      { index: true, element: <Home />, },
+      { path: "posts", element: <Posts />, },
+      { path: "movies", element: <Movies />, },
+      { path: "login", element: <Login />, },
+      { path: "signup", element: <Register />, },
+      { path: "users/search/:id", element: <Profile />, },
+      { path: "posts/search/:id", element: <PostWithComments />, },
+      { path: "*", element: <NotFound />, },
+    ],
+  },
+]);
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <RouterProvider router={router}>
+          <Layout />
+        </RouterProvider>
+      </Provider>
+
+    </div>
+  );
+}
+
+export default App;
